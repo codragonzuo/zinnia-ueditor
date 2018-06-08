3e0482ee1666e7c8d66339d7822c034637d215bf
codragonzuo <codragon@163.com>
2018-06-09 00:08:16 +0800

    Update settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = (
     'ueditor',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'my_site.urls'
 #ROOT_URLCONF = 'demo_zinnia_ckeditor.urls'
